ea214dea3af6118492cbdfda3fbf3c92de4c9ff6
Hamed Ghazali <hamedg20@yahoo.com>
2022-03-20 09:21:25 -0700

    max subarray

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import { betterFib } from './problems/fib.js';
 import { twoSums } from './problems/twoSums.js';
 import { maxProfit } from './problems/bestTimeBuySellStock.js';
 import { containsDuplicate } from './problems/containsDuplicate.js';
+import { maxSubArray } from './problems/maximumSubArray.js';
 
 // console.log(twoSums([2, 7, 11, 15], 9));
 // console.log('betterFib :>> ', betterFib(8));
 // console.log(maxProfit([7, 1, 5, 3, 6, 4]));
 // console.log('should be false ===>', containsDuplicate([1, 2, 3, 4]));
 // console.log('should be true ====>',containsDuplicate([0, 4, 5, 0, 3, 6]));
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
---
+++ problems/maximumSubArray.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/maximum-subarray/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+export const maxSubArray = (nums) => {
+  let maxCurrent = nums[0];
+  let maxGlobal = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    maxCurrent = Math.max(nums[i], maxCurrent + nums[i]);
+    if (maxCurrent > maxGlobal) {
+      maxGlobal = maxCurrent;
+    }
+  }
+  return maxGlobal;
+};
